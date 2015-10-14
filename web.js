b321fdf81bda161ed70038ff410ba7397e2362ff
Sébastien GLON <sg@keendo.fr>
2015-10-14 14:26:56 +0200

    upgrade to express 4*

--- web.js
+++ web.js
@@ -2,6 +2,4 @@ var gzippo = require('gzippo');
   var express = require('express');
   var app = express();
  
-  app.use(express.logger('dev'));
-  app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-  app.listen(process.env.PORT || 5000);
+app.listen(8080);
