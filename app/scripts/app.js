5ddfe6d5a0883dc4b813acdbcd0bd57d2cbb6ecc
Sébastien GLON <sg@keendo.fr>
2015-10-14 09:18:36 +0200

    initial commit . \n Yeoman generation and adding pouchdb

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name composeJsApp
+ * @description
+ * # composeJsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('composeJsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'pouchdb'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name composeJsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the composeJsApp
+ */
+angular.module('composeJsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
