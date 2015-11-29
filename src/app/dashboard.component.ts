365816afdf3ee5ee4700775fcc44b67b3887f250
Sébastien GLON <sg@keendo.fr>
2015-11-29 21:26:43 +0100

    correct compile

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -43,7 +43,8 @@ export class DashboardComponent implements OnInit {
      */
     getActiveApps() {
         // TODO call service
-
+        var result:DockApp[];
+        return result;
     }
 
     getHostVersion() {
